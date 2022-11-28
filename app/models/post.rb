9d1cbda565e195a51d0be228e49b123d5e44b829
Sam Arita <arita.sam@student.moringaschool.com>
2022-11-28 14:51:45 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_clickbait?
+    
+    Clickbait_patterns = [ /Won't Believe/,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i]
+
+    def title_clickbait?
+        if Clickbait_patterns.none? {|details| details.match title}
+        errors.add(:title, "must be clickbait")
+        end
+    end
+           
 end
